63c5dcb997d1353943e3fb4679ca1b41540012f8
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-05-16 18:01:58 +0300

    3.Input setup

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+
+
+export const featchWeater = async (region) => 
+await axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=cb8874ebb1344d34817173858220805&q=${region}&aqi=no`
+      )
+     
